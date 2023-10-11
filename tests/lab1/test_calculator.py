1f62cb2518e3a4562179812b4981685b90826eb2
Toneryy <144139291+Toneryy@users.noreply.github.com>
2023-10-11 11:36:09 +0300

    Update test_calculator.py

--- tests/lab1/test_calculator.py
+++ tests/lab1/test_calculator.py
@@ -1,7 +1,48 @@
-import unittest
+from unittest import TestCase
+from tests.lab1.calculator import calculator
 
-class CalculatorTestCase(unittest.TestCase):
 
-    # Тест для проверки работы, можно удалить
-    def test_one(self):
-        self.assertEquals(1, 1)
+class CalculatorTestCase(TestCase):
+    def test_1(self) -> None:
+        self.assertEqual(calculator('СТОП'), 'Вы закончили работу с калькулятором!')
+
+    def test_2(self) -> None:
+        self.assertEqual(calculator('5+6-81/6'), '-2.5')
+
+    def test_3(self) -> None:
+        self.assertEqual(calculator('5/'), 'Неизвестный ввод')
+
+    def test_4(self) -> None:
+        self.assertEqual(calculator('выпривпр'), 'Вы ввели не 2 числа, программа будет запущена заново')
+
+    def test_5(self) -> None:
+        self.assertEqual(calculator('sin'),
+                          'Вы ввели больше 1 числа, либо операции для данного числа не существует, программа будет запущена заново')
+
+    def test_6(self) -> None:
+        self.assertEqual(calculator('sin()'),
+                          'В функции sin(), cos(), tg(), ctg(), sqrt() нужно передать один аргумент!')
+
+    def test_7(self) -> None:
+        self.assertEqual(calculator('ctg(225'), '1.0000000000000004')
+
+    def test_8(self) -> None:
+        self.assertEqual(calculator('5(76-5)'), 'Вы ввели не 2 числа, программа будет запущена заново')
+
+    def test_9(self) -> None:
+        self.assertEqual(calculator('6541654654654654646*64516'), '422041391699699699141336')
+
+    def test_10(self) -> None:
+        self.assertEqual(calculator('ctg(180)'), '-8165619676597685.0')
+
+    def test_11(self) -> None:
+        self.assertEqual(calculator('sqrt(-4*8)'), 'Нельзя вычислить квадратный корень из отрицательного числа!')
+
+    def test_12(self) -> None:
+        self.assertEqual(calculator('sqrt(-)'), 'Вы ввели больше 1 числа, либо отрицательное число, программа будет запущена заново')
+
+    def test_13(self) -> None:
+        self.assertEqual(calculator('500/0'), 'Нельзя делить на ноль!')
+
+    def test_14(self) -> None:
+        self.assertEqual(calculator('15*sqrt(225)-666*sin(0)'), 'Вы ввели больше 1 числа, либо операции для данного числа не существует, программа будет запущена заново')
